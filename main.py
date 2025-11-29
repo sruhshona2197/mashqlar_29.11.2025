3c2c7a43b9711c4b0e0dcf4f799a4e5d0c6a94c5
sruhshona2197 <sruhshona2197@gmail.com>
2025-11-29 16:48:37 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+# 1-mashq
+kop  = lambda son: son * 5
+
+res = kop(20)
+print(res)
+
+#2-mashq
+kopaytma = lambda a, b: a * b
+
+print(kopaytma(4, 6))
+
+# 3-mashq
+toq_juft = lambda n: "juft" if n % 2 == 0 else "toq"
+
+print(toq_juft(2))
+
+# 4-mashq
+katta = lambda a, b: a if a > b else b
+
+print(katta(8, 3))
+print(katta(5, 12))
+
+
+# 5-mashq
+uzunmi = lambda ism: True if len(ism) > 7 else False
+
+print(uzunmi("Abdulloh"))
+print(uzunmi("Ali"))
+
